2b2f581724cdea50637fbfb290a88d66d83caac9
Lee Faus <leefaus@github.com>
2017-10-24 04:18:44 -0400

    initial commit

---
+++ Bingo.elm
@@ -0,0 +1,39 @@
+module Bingo exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+-- main =
+  -- Html.text (String.repeat 3 (String.toUpper "Lee's Game #3"))
+
+playerInfo name gameNumber =
+  name ++ " - Game #" ++ (toString gameNumber)
+
+viewPlayer name gameNumber =
+  let
+    playerInfoText =
+      playerInfo name gameNumber
+        |> String.toUpper
+        |> text
+  in
+    h2 [id "info", class "classy"] [playerInfoText]
+
+viewHeader title =
+  header []
+    [h1 [] [ text title] ]
+
+viewFooter =
+  footer []
+    [a  [href "http://elm-lang.org"]
+        [text "Powered by Elm"]
+    ]
+
+view =
+  div [class "content"]
+      [viewHeader "Buzzword Bingo",
+       viewPlayer "Buddy" 3,
+       viewFooter
+      ]
+
+main =
+  view
